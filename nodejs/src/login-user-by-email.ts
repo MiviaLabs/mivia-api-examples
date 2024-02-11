d509085101a599327751732fa227677759900701
Mac <mac@mivia.net>
2024-02-11 22:00:54 +0800

    updated example response

--- nodejs/src/login-user-by-email.ts
+++ nodejs/src/login-user-by-email.ts
@@ -6,12 +6,26 @@ import { init } from ".";
     init();
 
     /// Login USER by email and password ///
-    const res = await MiviaApi.auth.loginByEmail("test@test.com", "test");
+    const res = await MiviaApi.auth.loginByEmail("mac@mivia.net", "test");
 
     console.log(
       `User successfully logged in. \nAuthentication token: ${res.data.jwtToken}`
     );
+
+    // Example response:
+    // {
+    //   data: {
+    //     jwtToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyMzExMjM2Mi1jOTk0LTQ5ZDMtYmY0OS04YjA3ZmYzMWYyNGUiLCJhdXRoIjoiMTM1OWY5MTUtNWFhMC00MDY2LTk1ZWMtMTkwYjk1YmI4Mjg5IiwiaWF0IjoxNzA3NjU5MjE0fQ.RNyMlQLdAL16cXIGfiV8FwJtKbT5dhRENDNPN5AukCQ'
+    //   }
+    // }
   } catch (e: any) {
-    console.error(e);
+    console.log(JSON.parse(e.message));
+
+    // Example error:
+    // {
+    //   message: 'Invalid email and/or password',
+    //   error: 'Forbidden',
+    //   statusCode: 403
+    // }
   }
 })();
